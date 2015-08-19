c79bc6c7b67b07911da9d609909f234f056893b8
Scrivs <scrivs@gmail.com>
2015-08-19 10:46:03 -0700

    add admin controllers

---
+++ app/Http/Controllers/Admin/UploadController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Makersblog\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use Makersblog\Http\Requests;
+use Makersblog\Http\Controllers\Controller;
+
+class UploadController extends Controller
+{
+    //
+}
