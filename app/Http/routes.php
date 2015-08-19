f12f9b47d96dea82ad6f8a619131772d62c1bb5f
Scrivs <scrivs@gmail.com>
2015-08-19 10:37:09 -0700

    add Admin routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,18 @@ Route::get('/', function () {
 
 Route::get('blog', 'BlogController@index');
 Route::get('blog/{slug}', 'BlogController@showPost');
+
+Route::get('admin', function() {
+	return redirect('/admin/post');
+});
+
+Route::group(['namespace' => 'Admin', 'middleware' => 'auth'], function() {
+	Route::get('admin/upload', 'UploadController@index');
+	Route::resource('admin/post', 'PostContoller');
+	Route::resource('admin/tag', 'TagController');
+
+});
+
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
