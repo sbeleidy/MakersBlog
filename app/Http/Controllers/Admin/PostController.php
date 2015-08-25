a025b6328eaae4a75a2ed9ca4c1c695c3024f212
Scrivs <scrivs@gmail.com>
2015-08-25 13:25:13 -0700

    add post controller

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,8 +4,12 @@ namespace Makersblog\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 
+use Makersblog\Jobs\PostFormFields;
 use Makersblog\Http\Requests;
+use Makersblog\Http\Requests\PostCreateRequest;
+use Makersblog\Http\Requests\PostUpdateRequest;
 use Makersblog\Http\Controllers\Controller;
+use Makersblog\Post;
 
 class PostController extends Controller
 {
@@ -16,7 +20,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('admin.post.index');
+        return view('admin.post.index')->withPosts(Post::all());
     }
 
     /**
@@ -26,7 +30,9 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $data = $this->dispatch(new PostFormFields());
+
+        return view('admin.post.create', $data);
     }
 
     /**
@@ -35,9 +41,14 @@ class PostController extends Controller
      * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        //
+        $post = Post::create($request->postFillData());
+        $post->syncTags($request->get('tags', []));
+
+        return redirect()
+            ->route('admin.post.index')
+            ->withSuccess('New Post Successfully Created.');
     }
 
     /**
@@ -59,7 +70,9 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $data = $this->dispatch(new PostFormFields($id));
+
+        return view('admin.post.edit', $data);
     }
 
     /**
@@ -71,7 +84,20 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->fill($request->postFillData());
+        $post->save();
+        $post->syncTags($request->get('tags', []));
+
+        if ($request->action === 'continue') {
+            return redirect()
+                ->back()
+                ->withSuccess('Post saved.');
+        }
+
+        return redirect()
+            ->route('admin.post.index')
+            ->withSuccess('Post saved.');
     }
 
     /**
@@ -82,6 +108,12 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->tags()->detach();
+        $post->delete();
+
+        return redirect()
+            ->route('admin.post.index')
+            ->withSuccess('Post deleted.');
     }
 }
