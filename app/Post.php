f75f5c9e7c2155cf9845f0de18cdbba9c4ba9aa2
Scrivs <scrivs@gmail.com>
2015-08-25 10:07:27 -0700

    add markdowner

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,22 @@
 
 namespace Makersblog;
 
+use Makersblog\Services\Markdowner;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $dates = ['published_at'];
+
+    /**
+     * The many-to-many relationship between posts and tags
+     * 
+     * @return BelongsToMany
+     */
+    public function tags()
+    {
+    	return $this->belongsToMany('Makersblog\Tag', 'post_tag_pivot');
+    }
 
     // setNameAttribute
 
@@ -15,7 +26,59 @@ class Post extends Model
     	$this->attributes['title'] = $value;
 
     	if (! $this->exists) {
-    		$this->attributes['slug'] = str_slug($value, "-");
+    		//$this->attributes['slug'] = str_slug($value, "-");
+    		$this->setUniqueSlug($value, '');
     	} 
+    }
+
+    /** 
+     * Recursive routine to set a unique slug
+     * 
+     * @param string $title
+     * @param mixed $extra
+     */
+    protected function setUniqueSlug($title, $extra)
+    {
+    	$slug = str_slug($title.'-'.$extra);
+
+    	if (static::whereSlug($slug)->exists()) {
+    		$this->setUniqueSlug($title, $extra + 1);
+    		return;
+    	}
+
+    	$this->attributes['slug'] = $slug;
+
+    }
+
+    /**
+     * Set the HTML content automatically when the raw content is set
+     * 
+     * @param string $value
+     */
+    public function setContentRawAttribute($value)
+    {
+    	$markdown = new Markdowner();
+
+    	$this->attributes['content_raw'] = $value;
+    	$this->attributes['content_html'] = $markdown->toHTML($value);
+    }
+
+    /**
+     * Sync tag relation adding new tags as needed
+     * 
+     * @param array $tags
+     */
+    public function syncTags(array $tags)
+    {
+    	Tag::addNeededTags($tags);
+
+    	if (count($tags)) {
+    		$this->tags()->sync(
+    			Tag::whereIn('tag', $tags)->lists('id')->all();
+    		);
+    		return;
+    	}
+
+    	$this->tags()->detach();
     }
 }
